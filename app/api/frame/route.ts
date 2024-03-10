bf966ec6409c02536c453be3cff96a0680c4cf04
laitanop <laitanop@yahoo.com>
2024-03-10 14:44:49 -0600

    fix next response

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -19,22 +19,22 @@ async function getResponse(req: NextRequest) {
     </head>/html>`);
   }
   if (idAsNumber === 1) {
-    `<DOCTYPE html><html><head>
+    return new NextResponse(`<DOCTYPE html><html><head>
     <title>This is a frame ${id} </title>
     <meta name="fc:frame" content="vNext" />
     <meta name="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QQmPaXA9nd5mh4QxQoSFoLduEogKvs47yP1wfx4AincGvZn" />
     <meta name="fc:frame:button:1" content="Next Page" />
     <meta name="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
-    </head>/html>`;
+    </head>/html>`);
   }
   if (idAsNumber === 2) {
-    `<DOCTYPE html><html><head>
+    return new NextResponse(`<DOCTYPE html><html><head>
     <title>This is a frame ${id} </title>
     <meta name="fc:frame" content="vNext" />
     <meta name="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmcgTJdMZNukCuQQ3gz398WWtuCVuwfMPWaM4LT5NhkjRx" />
     <meta name="fc:frame:button:1" content="Next Page" />
     <meta name="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
-    </head>/html>`;
+    </head>/html>`);
   }
 }
 
